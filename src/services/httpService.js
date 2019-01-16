e4e79db7ae216dc34e55bb2d612bb4e4c473460a
ASOL\jma <jma@alpha-solutions.dk>
2019-01-16 16:15:52 +0100

    Logging Errors

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,12 +1,15 @@
 import axios from 'axios';
 import { toast } from 'react-toastify';
+import * as Sentry from '@sentry/browser';
 
 axios.interceptors.response.use(null, error => {
     const expectedError = error.response && error.response.status >= 400 && error.response.status < 500;
   
     if (!expectedError){
-      console.log('Logging the error', error);
-      toast.error('An unexpected error occurred.');
+         Sentry.captureException(error);
+         toast.error("An unexpected error occured"); 
+    //   console.log('Logging the error', error);
+    //   toast.error('An unexpected error occurred.');
     }
       return Promise.reject(error);
   });
