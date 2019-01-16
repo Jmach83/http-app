dc1c504137c3d513cd0a9ed24efdc498bead0638
ASOL\jma <jma@alpha-solutions.dk>
2019-01-16 14:59:54 +0100

    Handling Unexpected Errors Globally

--- src/App.js
+++ src/App.js
@@ -2,6 +2,16 @@ import React, { Component } from "react";
 import axios from 'axios';
 import "./App.css";
 
+axios.interceptors.response.use(null, error => {
+  const expectedError = error.response && error.response.status >= 400 && error.response.status < 500;
+
+  if (!expectedError){
+    console.log('Logging the error', error);
+    alert('An unexpected error occurred.');
+  }
+    return Promise.reject(error);
+})
+
 const apiEndpoint = 'http://jsonplaceholder.typicode.com/posts';
 
 class App extends Component {
@@ -42,17 +52,7 @@ class App extends Component {
     try {
       await axios.delete(apiEndpoint + '/' + post.id);    
     } catch (ex) {
-      // Expected (404: not found, 400: bad request) - CLIENT ERRORS
-      // - Display a specific error message
-      if(ex.response && ex.response.status === 404) alert('This post has already been deleted.');
-      else{
-        console.log('Logging the error', ex);
-        alert('An unexpected error occured');
-      }
-      //
-      // Unexpected (network down, server down, db down, bug)
-      // - Log them
-      // - Display a generic and friendly error message
+      if(ex.response && (ex.response.status === 404)) alert('This post has already been deleted.');
       this.setState({ posts: originalPosts });
     }
   };
